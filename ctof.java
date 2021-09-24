039a7b09f71e26f66b700bfd268007d26d7e774f
Aiden Li <ali30@stuy.edu>
2021-09-23 23:57:58 -0400

    f to c

--- ctof.java
+++ ctof.java
@@ -2,6 +2,9 @@ public class ctof{
   public static double celsiusToFahrenheit(double temp){
     return temp*1.8+32;
   }
+  public static double fahrenheitToCelsius(double temp){
+    return (temp-32)/1.8;
+  }
   public static void main(String[] args) {
 
   }
